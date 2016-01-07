669e73a1d1f292e6c4b9641899565352feb25a2f
hkaradi1 <hkaradi1@binghamton.edu>
2016-01-07 03:09:25 -0500

    created project and added header.jsx

---
+++ README.md
@@ -0,0 +1,4 @@
+	> git clone git@github.com:hkaradi1/ImgurReactImpl.git
+	> cd ImgurReactImpl
+	> npm install
+	> gulp
---
+++ src/components/main.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var Header = require('./header')
+var Topics = require('./topic-list');
+
+module.exports = React.createClass({
+
+	render : function () {
+		return <div> 
+			<Header/>
+			{this.content()}
+			
+		</div>
+	},
+	content: function() {
+		if(this.props.children) {
+			return this.props.children;
+		}
+
+		return <Topics/>
+	}
+});
